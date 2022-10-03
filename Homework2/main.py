eb46e8d5bd03b144b82a152b8efa1647b529ece0
NielsSchelleman <niels.schelleman@gmail.com>
2022-10-03 20:02:06 +0200

    2a and 2b

--- Homework2/main.py
+++ Homework2/main.py
@@ -9,12 +9,13 @@ def compute_mi(cols):
     cols[:, 0] *= 2
     names, occurs = np.unique(np.sum(cols, axis=1), return_counts=True)
     namelen = len(names)
-    # if only 2 marginals, no mutual information can be shared
+    # if only 2 marginals, variables must be independent so mutual information = 0
     if namelen == 2:
         return 0
-        # with three we need to put 1 of the functions to give a result of 0
+
+    # Use small value alpha to substitute the 0-values
     if namelen == 3:
-        occurs = [occurs[i] if (i in names) else 0 for i in range(4)]
+        occurs = np.insert(occurs.astype('float'), np.argmin(np.isin([0, 1, 2, 3], names)), 0.01)
 
     probs = np.log(occurs / sum(occurs))
 
@@ -23,35 +24,12 @@ def compute_mi(cols):
     py0 = logsumexp([probs[0], probs[2]])
     py1 = logsumexp([probs[1], probs[3]])
 
-    if namelen == 4:
-        mi = np.exp(probs[0]) * (probs[0] - logsumexp([px0, py0])) + \
-             np.exp(probs[1]) * (probs[1] - logsumexp([px0, py1])) + \
-             np.exp(probs[2]) * (probs[2] - logsumexp([px1, py0])) + \
-             np.exp(probs[3]) * (probs[3] - logsumexp([px1, py1]))
+    mi = np.exp(probs[0]) * (probs[0] - (px0 + py0)) + \
+         np.exp(probs[1]) * (probs[1] - (px0 + py1)) + \
+         np.exp(probs[2]) * (probs[2] - (px1 + py0)) + \
+         np.exp(probs[3]) * (probs[3] - (px1 + py1))
 
-    # results for one marginal being 0
-    else:
-        if probs[0] == 0:
-            p0 = 0
-        else:
-            p0 = np.exp(probs[0]) * (probs[0] - logsumexp([px0, py0]))
-
-        if probs[1] == 0:
-            p1 = 0
-        else:
-            p1 = np.exp(probs[1]) * (probs[1] - logsumexp([px0, py1]))
-
-        if probs[2] == 0:
-            p2 = 0
-        else:
-            p2 = np.exp(probs[2]) * (probs[2] - logsumexp([px1, py0]))
-
-        if probs[3] == 0:
-            p3 = 0
-        else:
-            p3 = np.exp(probs[3]) * (probs[3] - logsumexp([px1, py1]))
-        return p0 + p1 + p2 + p3
-    return mi
+    return np.log(mi)
 
 
 def prob_col(col, val, alpha):
@@ -81,10 +59,7 @@ class BinaryCLT:
             [[compute_mi(self.data[:, [i, j]]) if j > i else 0 for j in range(self.cols)] for i in range(self.cols)])
         # invert the mutual information
         # to get the maximum spanning tree by calculating the minimum spanning tree of the inverse
-        # exit the log domain as the inversion requires strict positivity
-        mi_matrix_inv = 1 / mi_matrix
-
-        mi_matrix_inv[mi_matrix_inv == np.inf] = 0
+        mi_matrix_inv = -mi_matrix
         tree = mst(mi_matrix_inv)
         # add connections to the tree in both directions
         tree = tree.toarray().astype(float)
