42f65874ef85f65805c2ed1aa66a6afed8ecd534
Mathieu-van-Luijken <79520315+Mathieu-van-Luijken@users.noreply.github.com>
2022-10-06 12:19:44 +0200

    exhaustive

--- Homework2/main.py
+++ Homework2/main.py
@@ -1,9 +1,12 @@
+from array import array
+from unittest import skip
 from scipy.sparse.csgraph import minimum_spanning_tree as mst
 from scipy.sparse.csgraph import breadth_first_order as bfo
 from scipy.special import logsumexp
 import numpy as np
 import csv
-
+import os
+import sys
 
 def prob_col(col, val, alpha):
     return np.log(2*alpha+len(col[col == val]))-np.log(4*alpha+len(col))
@@ -38,6 +41,24 @@ def compute_mi(cols, alpha):
     mi = sum([compute_partial_mi(cols, i, j, alpha) for i in range(2) for j in range(2)])
 
     return np.log(mi)
+
+
+
+def sum_out(matrix, pair):
+    #Dependant on which value is nan sum out the probability matrix
+    if np.isnan(pair[1]):
+        return matrix[0] + matrix[1]
+    elif np.isnan(pair[0]):
+        return matrix[:,0] + matrix[:,1]
+    else:
+        return matrix
+
+def summed_pmfs(pmfs, pairs):
+    #Build a tree out of all of the probability matrices
+    sum_pmfs = []
+    for i in range(len(pairs)):
+        sum_pmfs.append(sum_out(pmfs[i], pairs[i]))
+    return np.array(sum_pmfs)
 
 
 class BinaryCLT:
@@ -81,15 +102,66 @@ class BinaryCLT:
 
         return np.array(pmfs)
 
-    # def logprob(self, x, exhaustive: bool = False):
+    def logprob(self, x=np.array([]), exhaustive: bool = True):
+        #The answers for this specific example are in HW2 therefore im using this pmfs and tree as data
+        lp = []
+        self.pmfs = np.array([[[ -1.204 , -0.357] , [-1.204 , -0.357]] ,
+                    [[ -1.609 ,-0.223] , [ -0.511 , -0.916]] ,
+                    [[ -0.916 , -0.511] , [ -2.303 , -0.105]] ,
+                    [[ -0.223 , -1.609] , [ -0.693 , -0.693]] ,
+                    [[ -0.105 , -2.303] , [ -0.916 , -0.511]]])
+
+        self.tree = [ -1 , 0 , 4 , 4 , 0 ]
+        
+        if exhaustive:    
+            for sample in x:
+
+                #make [child value, parent value, index in pmfs]
+                pairs = np.array([[sample[i], sample[self.tree[i]], i] for i in range(len(sample))], dtype=np.float64)
+
+                #build a tree where nan values are summed out 
+                new_tree = summed_pmfs(np.exp(self.pmfs), pairs[:,:2])
+                print(new_tree)
+                probs = []
+
+                #for every pair take log of the correct value in the tree
+                for pair in pairs:
+                    #Probabilities of a fully nan matrix sum up to 2
+                    if np.isnan(pair[0]) and np.isnan(pair[1]):
+                        probs.append(np.log(2))
+                    #If child is Nan probabilities sum up to 1 (see tree)
+                    elif np.isnan(pair[0]):
+                        probs.append(np.log(1))
+                    #If parent is Nan use tree to get value
+                    elif np.isnan(pair[1]):
+                        probs.append(np.log(new_tree[int(pair[2])][int(pair[0])]))
+                    #If neither is nan use full matrix 
+                    else:
+                        probs.append(np.log(new_tree[int(pair[2])][int(pair[1])][int(pair[0])]))
+                    print(probs)
+                lp.append(np.sum(probs))
+            return lp
+        
+        
+        elif ~exhaustive:
+             
+
+
+
+            return None
+
 
     # def sample(self, nsamples: int)
 
 
 if __name__ == "__main__":
-    with open('nltcs.train.data', 'r') as file:
-        reader = csv.reader(file, delimiter=',')
+    with open(os.path.join(sys.path[0], 'nltcs.train.data'), 'r') as f:
+        reader = csv.reader(f, delimiter=',')
         dataset = np.array(list(reader)).astype(int)
+    with open(os.path.join(sys.path[0], 'nltcs_marginals.data'), 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        marginals = np.array(list(reader))
     mytree = BinaryCLT(dataset, 2)
-    print(mytree.pmfs)
-    print(mytree.tree)
+    # print(mytree.pmfs)
+    # print(mytree.tree)
+    print(mytree.logprob(x=np.array([[np.nan,0,np.nan,np.nan,1]])))
